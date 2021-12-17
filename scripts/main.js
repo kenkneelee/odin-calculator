5dfcb488a892fba18233a1e9166d27ae3e5e66f7
Kenny Lee <kenny.lee927@gmail.com>
2021-12-17 17:33:20 -0500

    clear display function, update display function

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
 /* --------------------Variables-------------------- */
 let displayValue = 0;
+const display = document.getElementById("display");
+
 
 /* ---------------------Script---------------------- */
 console.log(operate(divide, 40, 8));
 console.log(displayValue);
+document.onload = function () {
+    display.textContent = displayValue;
+    console.log(display.textContent)
+}
+
+/* ---------------------Buttons--------------------- */
+document.getElementById("clearButton").onclick = function () {
+    clear();
+    refresh();
+}
+
 
 /* --------------------Functions-------------------- */
+
+
+
 // add function
 function sum(a, b) {
     return a + b;
@@ -29,10 +45,15 @@ function divide(a,b) {
 //operate / equals button function
 function operate(operator, a, b) {
     displayValue = operator(a,b);
+    refresh();
     return operator(a,b);
 }
 
 //clear function
 function clear() {
     displayValue=0;
+}
+
+function refresh() {
+    display.textContent = displayValue;
 }
