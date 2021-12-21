004216c0bb000674e8b8a63a20910fba56818eae
Kenny Lee <kenny.lee927@gmail.com>
2021-12-20 22:46:48 -0500

    wrote operator button function

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 /* --------------------Variables-------------------- */
 let displayValue = 0;
+let inputValue = [];
+let currentOperator = "none";
 let a = [];
 let b = [];
 const display = document.getElementById("display");
@@ -13,34 +15,49 @@ window.onload = function () {
 }
 
 /* ---------------------Buttons--------------------- */
+// Clear button
 document.getElementById("clearButton").onclick = function () {
     clear();
     refresh();
 }
 
-const numerics = [...document.getElementsByClassName("numberButton")];
+// Backspace button
+const backspace = document.getElementById("backspaceButton");
+backspace.addEventListener(("click"), function () {
+    inputValue.pop();
+    displayValue = Number(inputValue.join(''));
+    refresh();
+});
 
+// Numeric buttons
+const numerics = [...document.getElementsByClassName("numberButton")];
 numerics.forEach(element => {
     element.addEventListener("click", function () {
-        a.push(element.id);
-        console.log(a);
-        displayValue = Number(a.join(''));
+        inputValue.push(element.id);
+        console.log(inputValue);
+        displayValue = Number(inputValue.join(''));
         refresh();
     })
 });
 
-const backspace = document.getElementById("backspaceButton");
-
-backspace.addEventListener(("click"), function () {
-    a.pop();
-    displayValue = Number(a.join(''));
-    refresh();
-});
+// Operator buttons
+const operators = [...document.getElementsByClassName("operatorButton")];
+operators.forEach(element => {
+    element.addEventListener("click", function () {
+        a = displayValue;
+        displayValue = 0;
+        refresh();
+        console.log(a);
+        inputValue = [];
+        currentOperator = element.id;
+        console.log(currentOperator);
+    })
+})
 
 
 /* --------------------Functions-------------------- */
 // add function
-function sum(a, b) {
+function add(a, b) {
     return a + b;
 };
 
@@ -64,13 +81,16 @@ function operate(operator, a, b) {
     displayValue = operator(a, b);
     refresh();
     return operator(a, b);
+
 }
 
 //clear function
 function clear() {
     a = [];
     b = [];
+    inputValue = [];
     displayValue = 0;
+    refresh();
 }
 
 function refresh() {
